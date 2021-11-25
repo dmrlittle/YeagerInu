ab513c1eadf76f50e0db2c33a1657966f2d87a27
dmrlittle <62012580+dmrlittle@users.noreply.github.com>
2021-11-25 23:41:14 +0530

    first testing

---
+++ Yeager.sol
@@ -0,0 +1,375 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.0;
+
+import { IERC20Metadata } from "./IERC20.sol";
+import { Context } from "./Context.sol";
+import { Ownable } from "./Ownable.sol";
+
+contract YeagerInu is Context, IERC20Metadata, Ownable {
+    
+    struct governingTaxes{
+        uint32 _split0;
+        uint32 _split1;
+        uint32 _split2;
+        uint32 _split3;
+        address _wallet1;
+        address _wallet2;
+    }
+
+    struct Fees {
+        uint256 _fee0;
+        uint256 _fee1;
+        uint256 _fee2;
+        uint256 _fee3;
+    }
+    
+    uint32 private _totalTaxPercent;
+    governingTaxes private _governingTaxes;
+    
+    mapping (address => uint256) private _rOwned;
+    mapping (address => uint256) private _tOwned;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    
+    mapping (address => bool) private _isExcluded;
+    address[] private _excluded;
+    mapping (address => bool) private _isExcludedFromFee;
+
+    mapping (address => bool) private _isBlacklisted;
+    uint256 public _maxTxAmount;
+
+    uint256 private constant _startingSupply = 100_000_000_000_000_000; //100 Quadrillion
+    
+    uint256 private constant MAX = ~uint256(0);
+    uint256 private constant _tTotal = _startingSupply * 10**18;
+    uint256 private _rTotal = (MAX - (MAX % _tTotal));
+    uint256 private _tFeeTotal;
+    
+    string private constant _name = 'Yeager Inu';
+    string private constant _symbol = 'YENU';
+    uint8 private constant _decimals = 18;
+
+    address public burnAddress = 0x000000000000000000000000000000000000dEaD; 
+
+    constructor (address wallet1_,  address wallet2_) {
+        _rOwned[_msgSender()] = _rTotal;
+
+        /*
+            Total Tax Percentage per Transaction : 10%
+            Tax Split:
+                > Burn (burnAddress): 10%
+                > Dev Wallet (wallet1): 20% 
+                > Marketing Wallet (wallet2): 50%
+                > Holders (reflect): 20%
+        */
+        _totalTaxPercent = 10;  
+        _governingTaxes = governingTaxes(10, 20, 50, 20, wallet1_, wallet2_); 
+        
+        //Max TX amount is 1% of the total supply.
+        _maxTxAmount = (_startingSupply / 100) * 10**18; 
+
+        //Excluding Owner and Other Governing Wallets From Reward System;
+        excludeFromFee(owner());
+        excludeFromReward(owner());
+        excludeFromReward(burnAddress);
+        excludeFromReward(wallet1_);
+        excludeFromReward(wallet2_);
+
+        emit Transfer(address(0), _msgSender(), _tTotal);
+    }
+
+    function name() public pure override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public pure override returns (uint256) {
+        return _tTotal;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        if (_isExcluded[account]) return _tOwned[account];
+        return tokenFromReflection(_rOwned[account]);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        unchecked {
+            _approve(sender, _msgSender(), currentAllowance - amount);
+        }
+
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        uint256 currentAllowance = _allowances[_msgSender()][spender];
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
+    }
+
+    function totalFees() public view returns (uint256) {
+        return _tFeeTotal;
+    }
+
+    function currentTaxes() public view 
+    returns (
+        uint32 Total_Tax_Percent,
+        uint32 Burn_Split,
+        uint32 GoverningSplit_Wallet1,
+        uint32 GoverningSplit_Wallet2,
+        uint32 Reflect_Split
+    ) {
+        return (
+            _totalTaxPercent,
+            _governingTaxes._split0,
+            _governingTaxes._split1,
+            _governingTaxes._split2,
+            _governingTaxes._split3
+        );
+    }
+
+    function isExcludedFromReward(address account) public view returns (bool) {
+        return _isExcluded[account];
+    }
+
+    function isExcludedFromFee(address account) public view returns(bool) {
+        return _isExcludedFromFee[account];
+    }
+
+    function isBlacklisted(address account) public view returns (bool) {
+        return _isBlacklisted[account];
+    }
+
+    function _hasLimits(address from, address to) private view returns (bool) {
+        return from != owner()
+            && to != owner()
+            && to != burnAddress;
+    }
+
+    function setBlacklistAccount(address account, bool enabled) external onlyOwner() {
+        _isBlacklisted[account] = enabled;
+    }
+
+    function setMaxTxAmount(uint256 maxTxAmount) external onlyOwner() {
+        require(maxTxAmount >= (_tTotal / 1000), "Max Transaction amt must be above 0.1% of total supply."); // Cannot set lower than 0.1%
+        _maxTxAmount = maxTxAmount;
+    }
+
+    function setTaxes(
+        uint32 totalTaxPercent_, 
+        uint32 split0_, 
+        uint32 split1_, 
+        uint32 split2_, 
+        uint32 split3_, 
+        address wallet1_, 
+        address wallet2_
+    ) external onlyOwner() {
+        require(wallet1_ != address(0) && wallet2_ != address(0), "Tax Wallets assigned zero address !");
+        require(totalTaxPercent_ <= 10, "Total Tax Percent Exceeded 10% !"); // Prevents owner from manipulating Tax.
+        require(split0_+split1_+split2_+split3_ == 100, "Split Percentages does not sum upto 100 !");
+
+        _totalTaxPercent = totalTaxPercent_;
+        _governingTaxes._split0 = split0_;
+        _governingTaxes._split1 = split1_;
+        _governingTaxes._split2 = split2_;
+        _governingTaxes._split3 = split3_;
+        _governingTaxes._wallet1 = wallet1_;
+        _governingTaxes._wallet2 = wallet2_;
+    }
+
+    function excludeFromFee(address account) public onlyOwner {
+        _isExcludedFromFee[account] = true;
+    }
+    
+    function includeInFee(address account) external onlyOwner {
+        _isExcludedFromFee[account] = false;
+    }
+
+    function excludeFromReward(address account) public onlyOwner() {
+        require(!_isExcluded[account], "Account is already excluded");
+        if(_rOwned[account] > 0) {
+            _tOwned[account] = tokenFromReflection(_rOwned[account]);
+        }
+        _isExcluded[account] = true;
+        _excluded.push(account);
+    }
+
+    function includeInReward(address account) external onlyOwner() {
+        require(_isExcluded[account], "Account is already excluded");
+        for (uint256 i = 0; i < _excluded.length; i++) {
+            if (_excluded[i] == account) {
+                _excluded[i] = _excluded[_excluded.length - 1];
+                _tOwned[account] = 0;
+                _isExcluded[account] = false;
+                _excluded.pop();
+                break;
+            }
+        }
+    }
+
+    function reflect(uint256 tAmount) public {
+        address sender = _msgSender();
+        require(!_isExcluded[sender], "Excluded addresses cannot call this function");
+        (uint256 rAmount,,,,) = _getValues(tAmount);
+        _rOwned[sender] = _rOwned[sender] - rAmount;
+        _rTotal = _rTotal - rAmount;
+        _tFeeTotal = _tFeeTotal + tAmount;
+    }
+
+    function reflectionFromToken(uint256 tAmount, bool deductTransferFee) public view returns(uint256) {
+        require(tAmount <= _tTotal, "Amount must be less than supply");
+        if (!deductTransferFee) {
+            (uint256 rAmount,,,,) = _getValues(tAmount);
+            return rAmount;
+        } else {
+            (,uint256 rTransferAmount,,,) = _getValues(tAmount);
+            return rTransferAmount;
+        }
+    }
+
+    function tokenFromReflection(uint256 rAmount) public view returns(uint256) {
+        require(rAmount <= _rTotal, "Amount must be less than total reflections");
+        uint256 currentRate =  _getRate();
+        return rAmount / currentRate;
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 tAmount
+    ) private {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        if(_hasLimits(sender, recipient)) {
+            require(tAmount <= _maxTxAmount, "Transfer amount exceeds the maxTxAmount.");
+            require(!isBlacklisted(sender) || !isBlacklisted(recipient), "Sniper Rejected");
+        }
+
+        uint32 _previoustotalTaxPercent;
+        if(_isExcludedFromFee[sender] || _isExcludedFromFee[recipient]) //checking if Tax should be deducted from transfer
+        {
+            _previoustotalTaxPercent = _totalTaxPercent;
+            _totalTaxPercent = 0; //removing Taxes
+        }
+
+        (uint256 rAmount, uint256 rTransferAmount, Fees memory rFee, uint256 tTransferAmount, Fees memory tFee) = _getValues(tAmount);
+
+        if(_isExcludedFromFee[sender] || _isExcludedFromFee[recipient]) _totalTaxPercent = _previoustotalTaxPercent; //restoring Taxes
+
+        _rOwned[sender] = _rOwned[sender] - rAmount;
+        _rOwned[recipient] = _rOwned[recipient] + rTransferAmount;
+
+        _rOwned[burnAddress] += rFee._fee0;
+        _rOwned[_governingTaxes._wallet1] += rFee._fee1;
+        _rOwned[_governingTaxes._wallet2] += rFee._fee2;
+        _reflectFee(rFee._fee3, tFee._fee3);
+
+        if (_isExcluded[sender]) _tOwned[sender] = _tOwned[sender] - tAmount;
+        if (_isExcluded[recipient]) _tOwned[recipient] = _tOwned[recipient] + tTransferAmount;
+        if (_isExcluded[address(0)]) _tOwned[burnAddress] += tFee._fee0;
+        if (_isExcluded[_governingTaxes._wallet1]) _tOwned[_governingTaxes._wallet1] += tFee._fee1;
+        if (_isExcluded[_governingTaxes._wallet2])_tOwned[_governingTaxes._wallet2] += tFee._fee2;
+        
+        emit Transfer(sender, recipient, tTransferAmount);
+    }
+
+    function _reflectFee(uint256 rFee, uint256 tFee) private {
+        _rTotal = _rTotal - rFee;
+        _tFeeTotal = _tFeeTotal + tFee;
+    }
+
+    function _getValues(uint256 tAmount) private view returns (uint256 rAmount, uint256 rTransferAmount, Fees memory rFee, uint256 tTransferAmount, Fees memory tFee) {
+        (tTransferAmount, tFee) = _getTValues(tAmount);
+        uint256 currentRate =  _getRate();
+        (rAmount, rTransferAmount, rFee) = _getRValues(tAmount, tFee, currentRate);
+        return (rAmount, rTransferAmount, rFee, tTransferAmount, tFee);
+    }
+
+    function _getTValues(uint256 tAmount) private view returns (uint256, Fees memory) {
+        Fees memory tFee;
+        tFee._fee0 = tAmount * _totalTaxPercent * _governingTaxes._split0 / 10**4;
+        tFee._fee1 = tAmount * _totalTaxPercent * _governingTaxes._split1 / 10**4;
+        tFee._fee2 = tAmount * _totalTaxPercent * _governingTaxes._split2 / 10**4;
+        tFee._fee3 = tAmount * _totalTaxPercent * _governingTaxes._split3 / 10**4;
+        uint256 tTransferAmount = tAmount - tFee._fee0 - tFee._fee1 - tFee._fee2 - tFee._fee3;
+        return (tTransferAmount, tFee);
+    }
+
+    function _getRValues(uint256 tAmount, Fees memory tFee, uint256 currentRate) private pure returns (uint256, uint256, Fees memory) {
+        uint256 rAmount = tAmount * currentRate;
+        Fees memory rFee;
+        rFee._fee0 = tFee._fee0 * currentRate;
+        rFee._fee1 = tFee._fee1 * currentRate;
+        rFee._fee2 = tFee._fee2 * currentRate;
+        rFee._fee3 = tFee._fee3 * currentRate;
+        uint256 rTransferAmount = rAmount - rFee._fee0 - rFee._fee1 - rFee._fee2 - rFee._fee3;
+        return (rAmount, rTransferAmount, rFee);
+    }
+
+    function _getRate() private view returns(uint256) {
+        (uint256 rSupply, uint256 tSupply) = _getCurrentSupply();
+        return rSupply / tSupply;
+    }
+
+    function _getCurrentSupply() private view returns(uint256, uint256) {
+        uint256 rSupply = _rTotal;
+        uint256 tSupply = _tTotal;      
+        for (uint256 i = 0; i < _excluded.length; i++) {
+            if (_rOwned[_excluded[i]] > rSupply || _tOwned[_excluded[i]] > tSupply) return (_rTotal, _tTotal);
+            rSupply = rSupply - _rOwned[_excluded[i]];
+            tSupply = tSupply - _tOwned[_excluded[i]];
+        }
+        if (rSupply < _rTotal / _tTotal) return (_rTotal, _tTotal);
+        return (rSupply, tSupply);
+    }
+}
