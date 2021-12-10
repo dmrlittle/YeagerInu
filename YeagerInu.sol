1673edc7dfad153a728ad8a39b8d65e59d87b34a
dmrlittle <62012580+dmrlittle@users.noreply.github.com>
2021-12-11 02:47:39 +0530

    all changes

--- YeagerInu.sol
+++ YeagerInu.sol
@@ -405,7 +405,9 @@ contract YeagerInu is Context, IERC20Metadata, Ownable {
         if(_hasLimits(sender, recipient)) {
             require(tAmount <= _maxTxAmount, "Transfer amount exceeds the maxTxAmount");
             require(!isBlacklisted(sender) || !isBlacklisted(recipient), "Sniper Rejected");
-            require(balanceOf(recipient)+tAmount <= _maxHoldAmount, "Receiver address exceeds the maxHoldAmount");
+            if(!_isLiquidityPool[recipient]) {
+                require(balanceOf(recipient)+tAmount <= _maxHoldAmount, "Receiver address exceeds the maxHoldAmount");
+            }
         }
 
         uint32 _previoustotalTaxPercent;
